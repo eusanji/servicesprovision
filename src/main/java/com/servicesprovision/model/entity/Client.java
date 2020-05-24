6dd6b2c1ecff24017811df4292fdc816bb7635b9
eusanji <40520586+eusanji@users.noreply.github.com>
2020-05-24 00:26:30 -0300

    Defining Client and Service entities.

--- src/main/java/com/servicesprovision/model/entity/Client.java
+++ src/main/java/com/servicesprovision/model/entity/Client.java
@@ -14,14 +14,14 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(nullable = false, length = 150)
     private String name;
+
     @Column(nullable = false, length = 11)
     private String cpf;
-    @Column
-    private LocalDate dateEnrolled;
 
-
-
+    @Column(name = "date_enroll")
+    private LocalDate dateEnroll;
 
 }
---
+++ src/main/java/com/servicesprovision/model/entity/Service.java
@@ -0,0 +1,26 @@
+package com.servicesprovision.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String description;
+
+    @Column
+    private BigDecimal value;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client")
+    private Client client;
+
+}
